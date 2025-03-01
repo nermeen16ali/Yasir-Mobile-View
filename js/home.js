3d38dc2498b910c48cb89ee56a9c926e224f13fd
nermeen16ali <nermeenali540@gmail.com>
2025-03-01 23:55:10 +0200

    d

---
+++ js/home.js
@@ -0,0 +1,55 @@
+window.addEventListener("scroll", function () {
+  let header = document.querySelector(".header");
+  let menu_icon = document.querySelector(".menu-icon");
+  if (window.scrollY > 12) {
+    menu_icon.classList.add("moveToTop");
+    header.classList.add("scrolled");
+  } else {
+    menu_icon.classList.remove("moveToTop");
+    header.classList.remove("scrolled");
+  }
+});
+document.querySelector(".learn_more").addEventListener("click", function (e) {
+  e.preventDefault();
+  window.scrollBy({
+    top: window.innerHeight / 30,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+const createOdometer = (el, value) => {
+  const odometer = new Odometer({
+    el: el,
+    value: 0,
+  });
+
+  let hasRun = false;
+
+  const options = {
+    threshold: [0, 0.9],
+  };
+
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (!hasRun) {
+          odometer.update(value);
+          hasRun = true;
+        }
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+  observer.observe(el);
+};
+
+const subscribersOdometer = document.querySelector(".treatment-odometer");
+createOdometer(subscribersOdometer, 876031);
+
+const videosOdometer = document.querySelector(".productivity-odometer");
+createOdometer(videosOdometer, 3060);
+
+const projectsOdometer = document.querySelector(".total-odometer");
+createOdometer(projectsOdometer, 71);
