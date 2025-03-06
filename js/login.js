8b9cc9d18199b29966e287cd219e0f5b8f617797
nermeen16ali <nermeenali540@gmail.com>
2025-03-06 17:28:25 +0200

    d

--- js/login.js
+++ js/login.js
@@ -1,3 +1,65 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const togglePassword = document.getElementById("togglePassword");
+//   const passwordField = document.getElementById("password");
+//   const toggleIcon = togglePassword.querySelector("img");
+
+//   togglePassword.addEventListener("click", function () {
+//     const isPassword = passwordField.type === "password";
+//     passwordField.type = isPassword ? "text" : "password";
+
+//     toggleIcon.src = isPassword
+//       ? "images/password-toggle.svg"
+//       : "images/password-toggle.svg";
+//     toggleIcon.alt = isPassword ? "Hide Password" : "Show Password";
+//   });
+// });
+
+// document.querySelectorAll(".close-all").forEach((button) => {
+//   button.addEventListener("click", function () {
+//     var modals = document.querySelectorAll(".modal.show");
+//     modals.forEach((modal) => {
+//       var bsModal = bootstrap.Modal.getInstance(modal);
+//       if (bsModal) bsModal.hide();
+//     });
+//   });
+// });
+// document.addEventListener("DOMContentLoaded", function () {
+//   const inputs = document.querySelectorAll(".otp-input");
+
+//   inputs.forEach((input, index) => {
+//     input.style.direction = "ltr";
+//     input.style.textAlign = "center";
+
+//     input.addEventListener("input", (e) => {
+//       e.target.value = e.target.value.replace(/\D/g, "");
+
+//       if (e.target.value.length === 1 && index < inputs.length - 1) {
+//         inputs[index + 1].focus();
+//       }
+//     });
+
+//     input.addEventListener("keydown", (e) => {
+//       if (e.key === "Backspace" && index > 0 && !e.target.value) {
+//         inputs[index - 1].focus();
+//       }
+//     });
+
+//     input.addEventListener("keypress", (e) => {
+//       if (!/\d/.test(e.key)) {
+//         e.preventDefault();
+//       }
+//     });
+//   });
+
+//   const otpModal = document.getElementById("choice1Modal");
+//   if (otpModal) {
+//     otpModal.addEventListener("shown.bs.modal", function () {
+//       const firstInput = document.querySelector(".otp-input");
+//       if (firstInput) firstInput.focus();
+//     });
+//   }
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
   const togglePassword = document.getElementById("togglePassword");
   const passwordField = document.getElementById("password");
@@ -8,12 +70,11 @@ document.addEventListener("DOMContentLoaded", function () {
     passwordField.type = isPassword ? "text" : "password";
 
     toggleIcon.src = isPassword
-      ? "images/password-toggle.svg"
-      : "images/password-toggle.svg";
+      ? "images/hidepass.svg"
+      : "images/password_toggle.svg";
     toggleIcon.alt = isPassword ? "Hide Password" : "Show Password";
   });
 });
-
 document.querySelectorAll(".close-all").forEach((button) => {
   button.addEventListener("click", function () {
     var modals = document.querySelectorAll(".modal.show");
@@ -27,12 +88,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputs = document.querySelectorAll(".otp-input");
 
   inputs.forEach((input, index) => {
-    input.style.direction = "ltr";
-    input.style.textAlign = "center";
-
     input.addEventListener("input", (e) => {
       e.target.value = e.target.value.replace(/\D/g, "");
-
       if (e.target.value.length === 1 && index < inputs.length - 1) {
         inputs[index + 1].focus();
       }
@@ -50,12 +107,17 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+});
 
+document.addEventListener("DOMContentLoaded", function () {
   const otpModal = document.getElementById("choice1Modal");
+  const firstInput = document.querySelector(".otp-input");
+
   if (otpModal) {
     otpModal.addEventListener("shown.bs.modal", function () {
-      const firstInput = document.querySelector(".otp-input");
-      if (firstInput) firstInput.focus();
+      if (firstInput) {
+        firstInput.focus();
+      }
     });
   }
 });
